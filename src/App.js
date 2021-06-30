6798e869d4f375954de3246b9fb25c6c45795db0
VSlobolinskyi <32908303+VSlobolinskyi@users.noreply.github.com>
2021-06-30 20:49:11 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="container">
       <h1 class="text">Stfinance Mājaslapa atrodas izstrādes stadijā.</h1>
-      <img class="image" src="https://cdn.discordapp.com/attachments/516262146095448085/859830384773824552/image0.png" />
+      <img class="image" src="https://cdn.discordapp.com/attachments/516262146095448085/859830384773824552/image0.png" alt="site is in developement"/>
     </div>
   );
 }

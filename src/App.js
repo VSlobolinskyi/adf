ff2dbf5936778d2adf1cdae9658b1416c56d4da0
VSlobolinskyi <32908303+VSlobolinskyi@users.noreply.github.com>
2021-06-30 20:29:43 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 class="text">Stfinance Mājaslapa atrodas izstrādes stadijā.</h1>
+      <img class="image" src="https://cdn.discordapp.com/attachments/516262146095448085/859830384773824552/image0.png" />
+    </div>
+  );
+}
+
+export default App;
